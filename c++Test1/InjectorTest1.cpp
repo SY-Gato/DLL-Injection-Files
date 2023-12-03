a266612104733821826065571d87ed245a3853a7
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-12-02 22:53:23 -0600

    Update InjectorTest1.cpp

--- c++Test1/InjectorTest1.cpp
+++ c++Test1/InjectorTest1.cpp
@@ -1,1 +1,9 @@
+#include <Windows.h>
+#inckude <TlHelp32.h>
 
+int getProcId(const wchar_t* target) {
+  DWORD pID = 0;
+  PROCESSENTRY32 pe32;
+  pe32.dwSize = sizeof(PROCESSENTRY32);
+  HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+}
